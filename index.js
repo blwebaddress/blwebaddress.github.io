8fd360da0e548e34aba07013b20f14fdf62a74f3
blueredcat <blueredcat@gmail.com>
2015-08-04 18:04:09 +1000

    Contact form completion. Footer. Waiting on cartoons.

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-//$(document).ready(
-//
-//    function() {
-//
-//             $(".contactform").on("submit", function(e) {
-//
-//                 e.preventDefault()
-//
-//                var email = $.trim( $(".conactemailinput").val() );
-//
-//                var message = $.trim( $(".conactemailmsginput").val() );
-//
-//                console.log(email);
-//
-//                console.log(message);
-//
-//                $.ajax({
-//                    url: "//formspree.io/blwebaddress@gmail.com",
-//                    method: "POST",
-//                    data: {email: email, message: message},
-//                    dataType: "json"
-//                });
-//
-//            });
-//    }
-//)
+$(document).ready(
+
+
+    function() {
+
+             var custdate = new Date();
+
+             $(".copyrightyear").text(custdate.getFullYear());
+
+             $(".contactform").on("submit", function(e) {
+
+                e.preventDefault();
+
+                var email = $.trim( $(".conactemailinput").val() );
+
+                var message = $.trim( $(".conactemailmsginput").val() );
+
+                console.log(email);
+
+                console.log(message);
+                if($.trim( $(".conactformtest").val() ) == "5") {
+
+                    $.ajax({
+                        url: "//formspree.io/blwebaddress@gmail.com",
+                        method: "POST",
+                        data: {email: email, message: message},
+                        dataType: "json",
+                        success: function (data, b, c) {
+                            if (data) {
+                                if (data.success) {
+                                    $(".successmessage").text(data.success);
+                                    $(".successmessage").removeClass("unsuccesful");
+                                    $(".successmessage").addClass("succesful");
+                                }
+                            }
+                        },
+                        error: function() {
+                            $(".successmessage").text("Error");
+                            $(".successmessage").removeClass("succesful");
+                            $(".successmessage").addClass("unsuccesful");
+                        }
+                    });
+                } else {
+                    $(".successmessage").text("Error");
+                    $(".successmessage").removeClass("succesful");
+                    $(".successmessage").addClass("unsuccesful");
+                }
+            });
+    }
+);
 
